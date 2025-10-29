1032402cc98fa3ce9f9a6cb3f8a0d074e15cfb54
juan gabriel martinez <jmartinez@puntacana.com>
2025-10-29 13:46:25 -0400

    Chapter 08 – Refactoring Index and About Pages

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,19 +5,22 @@ export class AppController {
   @Get('/')
   @Render('index')
   index() {
-    return { title: 'Welcome to the Online Store' };
+    const viewData = [];
+    viewData['title'] = 'Home Page - Online Store';
+    return {
+      viewData: viewData,
+    };
   }
 
   @Get('/about')
   @Render('about')
   about() {
     const viewData = [];
+    viewData['title'] = 'About us - Online Store';
+    viewData['subtitle'] = 'About us';
     viewData['description'] = 'This is an about page ...';
     viewData['author'] = 'Developed by: Gabriel Martinez';
-    const data1 = 'About us - Online Store';
     return {
-      title: data1,
-      subtitle: 'About us',
       viewData: viewData,
     };
   }
